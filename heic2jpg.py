e64819d55111cd35844108313fc80dc20a472372
VolodymyrVdovyn <42466029+VolodymyrVdovyn@users.noreply.github.com>
2020-08-07 15:45:00 +0300

    Update heic2jpg.py

--- heic2jpg.py
+++ heic2jpg.py
@@ -5,20 +5,18 @@ import shutil
 import re
 
 
-path = input()
+path = input('Absolute path to directory: ')
 
 
 def create_dir(name):
-	"""Create dir"""
-	d = os.path.join(path, name)
-	if os.path.exists(d):
-		if os.path.isdir(d):
-			pass
-		else:
-			os.remove(d)
-			os.mkdir(d)
-	else:
-		os.mkdir(d)
+    """Create dir"""
+    d = os.path.join(path, name)
+    if os.path.exists(d):
+        if not os.path.isdir(d):
+            os.remove(d)
+            os.mkdir(d)
+    else:
+        os.mkdir(d)
 
 
 create_dir('video')
@@ -30,29 +28,28 @@ pattern = r'.\w+$'
 
 lst = os.listdir(path)
 for i in lst:
-	inp = os.path.join(path, i)
-	file = re.search(pattern, i).group(0).lower()
-	if file in ['.mov', '.mp4']:
-		print(i)
-		outp = os.path.join(video, i)
-		shutil.move(inp, outp)
-	elif file in ['.png', '.jpg', '.jpeg']:
-		print(i)
-		outp = os.path.join(photo, i)
-		shutil.move(inp, outp)
-	elif file in ['.heic', 'heif']:
-		print(i)
-		outp = photo + '/' + i[:-5] + '.jpg'
-		heif_file = pyheif.read_heif(inp)
-		image = Image.frombytes(mode=heif_file.mode, size=heif_file.size, data=heif_file.data)
-		image.save(outp, "JPEG")
-		
-		"""Save heic files in new dir"""
-		make_dir('heic')
-		heic = os.path.join(path, 'heic')
-		outp = os.path.join(heic, i)
-		shutil.move(inp, outp)
+    inp = os.path.join(path, i)
+    file = re.search(pattern, i).group(0).lower()
+    if file in ['.mov', '.mp4']:
+        print(i)
+        outp = os.path.join(video, i)
+        shutil.move(inp, outp)
+    elif file in ['.png', '.jpg', '.jpeg']:
+        print(i)
+        outp = os.path.join(photo, i)
+        shutil.move(inp, outp)
+    elif file in ['.heic', 'heif']:
+        print(i)
+        outp = photo + '/' + i[:-5] + '.jpg'
+        heif_file = pyheif.read_heif(inp)
+        image = Image.frombytes(mode=heif_file.mode, size=heif_file.size, data=heif_file.data)
+        image.save(outp, "JPEG")
 
-		"""Or delete heic files"""
-		# os.remove(inp)
-	
+        """Save heic files in new dir"""
+        make_dir('heic')
+        heic = os.path.join(path, 'heic')
+        outp = os.path.join(heic, i)
+        shutil.move(inp, outp)
+
+        """Or delete heic files"""
+        # os.remove(inp)
